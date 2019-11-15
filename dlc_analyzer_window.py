f526ad727d437c0f0696355d8299ee25f74b19cf
Chase <chasedudas13@gmail.com>
2019-11-15 11:23:01 -0700

    Add file

---
+++ dlc_analyzer_window.py
@@ -0,0 +1,12 @@
+
+def main():
+    # Holds the user inputed file paths
+    fileArgs = ['D://Donaldson Lab/Current Work/Covariation of Movement/Similar Motion/Import 9-16/similar_motionDeepCut_resnet50_Normal_MotionAug14shuffle1_110000.csv']
+
+    # Pass files as arguments to the function; Return data structure with analysis metrics
+
+    # Show glimpses for windows under threshold
+    
+
+if __name__ == "__main__":
+    main()
